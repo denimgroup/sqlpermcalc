c09c62165c954ed59f0693b04d279a9db4622a91
Dan Cornell <dan@denimgroup.com>
2012-04-05 15:04:53 -0500

    -Fixed naming issue with unit_insert_sql -Added initial unit tests for SELECT queries

---
+++ tests/unit_select.sql
@@ -0,0 +1,6 @@
+SELECT * FROM Select_Wildcard
+SELECT singlecolumn FROM Select_Single
+SELECT column1, column2, column3 FROM Select_Three
+SELECT inselect FROM Select_OneInSelectOneInWhere WHERE inwhere = 1
+SELECT inselect1, inselect2 FROM Select_TwoInSelectTwoInWhereAnd WHERE inwhere1 = 1 AND inwhere2 = 2
+SELECT inselect1, inselect2 FROM Select_TwoInSelectTwoInWhereOr WHERE inwhere1 = 1 OR inwhere2 = 2
